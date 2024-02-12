db096bcb35fe31043261c58a3e5a099462eb50fc
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-02-12 17:11:42 +0530

    Update Shop.jsx

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -6,7 +6,7 @@ const Shop = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(()=>{
-    fetch("http://localhost:5000/all-books").then(res =>res.json()).then(data =>setBooks(data));
+    fetch("https://bookswap-4zmq.onrender.com/all-books").then(res =>res.json()).then(data =>setBooks(data));
   })
   return (
     <div className='mt-28 px-4 lg:px-24'>
