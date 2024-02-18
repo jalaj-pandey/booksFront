8e1547fd540ecc8fd8594ad95472dc8b5d294212
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-02-18 15:16:07 +0530

    Update SingleBookPage.jsx

--- src/shop/SingleBookPage.jsx
+++ src/shop/SingleBookPage.jsx
@@ -3,34 +3,35 @@ import { useLoaderData } from "react-router-dom";
 
 const SingleBookPage = () => {
     const { bookTitle, bookDescription,imageURL,bookPrice,category} = useLoaderData();
-  return (
-      <div className='px-4 lg:px-24 bg-green-100 flex items-center md-max-w-screen-md'>
-  <div className='flex flex-row w-full md:flex-row justify-between items-center gap-16 py-32'>
+  return(
+    <div className='px-4 lg:px-24 bg-green-100 flex flex-col items-center md-max-w-screen-md '>
+  <div className='w-full flex flex-col-reverse md:flex-row justify-between items-center gap-16 py-32 '>
     {/* LEFT SIDE */}
-    <div className=' sm:w-1/2 flex flex-col justify-around md:w-1/4 space-y-8 h-full'>
-      <img className="h-92 rounded" src={imageURL} alt="" />
-      <div className="w-full">
-        <button className='bg-rose-600 mx-20 px-8 py-3 text-white font-medium hover:bg-black transition-all ease-in duration-200'>
-          Buy Now
+    <div className='w-full md:w-1/3 flex flex-col justify-around text-center '>
+      <img className="h-92 rounded mx-auto" src={imageURL} alt="" />
+      <div className=" mt-10 -mb-16">
+        <button className='bg-rose-600 mx-4 px-8 py-3 text-white font-medium hover:bg-black transition-all ease-in duration-200 '>
+          Buy Now 
         </button>
       </div>
     </div>
 
     {/* RIGHT SIDE */}
-    <div className='sm:h-full -mx-28 -mt-48 md:w-3/4'>
-      <h2 className='text-2xl '> {category}</h2>
+    <div className='w-full -my-8 md:w-2/3 '>
       <h2 className='text-4xl leading-snug text-black'>
-        <span className='text-rose-600 font-bold'>{bookTitle}</span>
+      <span className='text-rose-600 font-bold'>{bookTitle}</span>
       </h2>
       <br />
-      <p className='text-2xl md:w-4/5'>{bookDescription}</p>
+      <p className='text-xl'>{bookDescription}</p>
       <br />
-      <h2 className='text-2xl font-bold'>₹{bookPrice}</h2>
-
+      <h2 className='text-2xl'>{category}</h2>
+      <h2 className='text-2xl font-bold'>Price: ₹{bookPrice}</h2>
     </div>
   </div>
 </div>
-  )
+
+  
+  )  
 };
 
 export default SingleBookPage;
