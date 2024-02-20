d877e09196c51ca8f30918426e809b87ff4c0a5b
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-02-20 15:39:35 +0530

    Update SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../contects/AuthProviders";
 import googleLogo from "../assets/google-logo.svg";
+import Navbar from "./Navbar";
 
 const SignUp = () => {
   const { createUser, loginWithGoogle } = useContext(AuthContext);
@@ -47,6 +48,8 @@ const SignUp = () => {
       });
   };
   return (
+    <>
+    <div><Navbar/></div>
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
@@ -119,6 +122,7 @@ const SignUp = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
