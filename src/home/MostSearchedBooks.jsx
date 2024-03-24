edaa32b01934658b0212da596a049b0e5c0c534f
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-03-24 15:57:22 +0530

    Update MostSearchedBooks.jsx

--- src/home/MostSearchedBooks.jsx
+++ src/home/MostSearchedBooks.jsx
@@ -4,11 +4,9 @@ import BookCards from '../components/BookCards';
 const MostSearchedBooks = () => {
     const [books, setBooks] = useState([]);
     useEffect( ()=>{
-<<<<<<< HEAD
-        fetch('http://192.168.137.1:5000/all-books').then(res => res.json()).then(data => setBooks(data.slice(0,6)))
-=======
+
         fetch('https://bookswap-4zmq.onrender.com/all-books').then(res => res.json()).then(data => setBooks(data.slice(0,6)))
->>>>>>> 011d601e4638ee1a54fba75e4da82433f7134ac2
+
     }, [])
   return (
     <div>
