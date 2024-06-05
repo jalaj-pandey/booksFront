46455ac6cf46fb6424ff7e3ef21762aa3611e3c6
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-06-05 18:11:29 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
     { link: "Home", path: "/" },
     { link: "About", path: "/about" },
     { link: "Shop", path: "/shop" },
-    { link: "Sell Your Book", path: "/admin/dashboard" },
+    { link: "Sell Your Book", path: "/admin/dashboard/upload" },
   ];
 
   return (
